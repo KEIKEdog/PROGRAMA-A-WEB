23fa3a3ee141ee41921c9bdd512fa51438d28747
kaique <kaiquesilva231999@gmail.com>
2026-06-10 20:08:24 +0000

    Criando uma Página de Perfil do Aluno

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -1,13 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+
 public class AlunoController : Controller
 {
     public IActionResult Index()
     {
         ViewBag.Nome = "Kaique";
-        ViewBag.Cusro = "Tecnico Em Analise e Desenvolvimento de Sistemas";
+        ViewBag.Curso = "Tecnico Em Analise e Desenvolvimento de Sistemas";
         ViewBag.Semestre = "Primeiro Semestre";
         return View();
     }
-
-     
-}
+};
